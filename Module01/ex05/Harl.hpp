789f34350c1b50dacd0364107029302a8f0a319d
Nicolo-Linux <NicoloRomito@protonmail.com>
2024-09-02 07:57:20 +0200

    temporary commit

---
+++ Module01/ex05/Harl.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+# include <iostream>
+# include <string>
+# include <cmath>
+
+class Harl {
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		Harl();
+		~Harl();
+
+		void	complain(std::string level);
+};
