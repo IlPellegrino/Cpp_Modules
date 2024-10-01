24540721e31f2b968b9d0e00ac67cd97dfc90e7c
Nico Linux Desktop <NicoloRomito@protonmail.com>
2024-10-01 21:28:16 +0200

    ex00 done

--- Module08/ex00/include/easyfind.hpp
+++ Module08/ex00/include/easyfind.hpp
@@ -1,8 +1,21 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <iostream>
+#include <vector>
+
 template <typename T>
-void	
+bool	easyfind(T& a, int b)
+{
+	for (std::vector<int>::iterator it = a.begin(); it != a.end(); it++) {
+		if (*it == b) {
+			std::cout << *it << " has been found!\nIt's the same as " << b << std::endl;
+			return true;
+		}
+	}
+	std::cerr << "No occurrence found!\n";
+	return false;
+}
 
 
 #endif
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -1,6 +1,19 @@
 #include "include/easyfind.hpp"
+#include <vector>
 
 int	main()
 {
-	
+	std::vector<int>	v;
+
+	v.push_back(100);
+	v.push_back(24);
+	v.push_back(31);
+	v.push_back(3);
+	v.push_back(10);
+	v.push_back(324);
+
+
+	if (!easyfind(v, 10))
+		return (1);
+	return (0);
 }
