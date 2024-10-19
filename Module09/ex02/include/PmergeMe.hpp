ce097990e7da64fc32d88322dba440835ff5fd29
Nicolo-Linux <NicoloRomito@protonmail.com>
2024-10-19 23:00:30 +0200

    ex02

---
+++ Module09/ex02/include/PmergeMe.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+
+
+class	PmergeMe {
+	private:
+		
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe& p);
+		PmergeMe& operator=(const PmergeMe& p);
+		~PmergeMe();
+
+		
+};
---
+++ Module09/ex02/main.cpp
@@ -0,0 +1,8 @@
+#include "include/PmergeMe.hpp"
+
+int	main(int ac, char **av)
+{
+	if (ac < 2) {
+		std::cerr << "Error: too few arguments\nUsage: <executable> and <numbers to sort>\n";
+	}
+}
